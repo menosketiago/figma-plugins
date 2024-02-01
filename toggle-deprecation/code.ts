7e95a4857f49aaa8c0bf403f86b6a1dac034a537
Tiago Almeida <tiago.almeida@volvocars.com>
2024-02-01 17:33:38 +0100

    Added the plugin to toggle deprecation layers on the page

---
+++ toggle-deprecation/code.ts
@@ -0,0 +1,16 @@
+// This plugin toggles the visibility of the 📦 Deprecating layer on a page
+const deprecatedComponentArray = figma.currentPage.findAll(n =>n.name.includes("[DEPRECATING]"));
+
+Array.from(deprecatedComponentArray).forEach((component) => {
+    if (component.type === "INSTANCE") {
+        const deprecationLayer = component.findChild(n => n.name === "📦 Deprecating");
+
+        if (deprecationLayer) {
+            deprecationLayer.visible = !deprecationLayer.visible;
+        }
+    }
+});
+
+// Make sure to close the plugin when you're done. Otherwise the plugin will
+// keep running, which shows the cancel button at the bottom of the screen.
+figma.closePlugin();
