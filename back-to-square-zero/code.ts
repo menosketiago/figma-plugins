7f7a602722120a2c837c692a71dd98b8873e0200
Tiago Almeida <tiago.almeida@volvocars.com>
2024-02-14 11:01:29 +0100

    Added the back to square zero plugin

---
+++ back-to-square-zero/code.ts
@@ -0,0 +1,17 @@
+// This plugin will group everything on the page and set the group 
+// to x=0 and y=0 (Figma canvas origin point and default zoom position).
+const pageContent = figma.currentPage.children;
+
+const group = figma.group(pageContent, figma.currentPage);
+
+group.name = "Temporary group";
+group.x = 0;
+group.y = 0;
+
+if (figma.currentPage.findChildren(n => n.name === "Temporary group")) {
+    figma.ungroup(group, figma.currentPage);
+}
+
+// Make sure to close the plugin when you're done. Otherwise the plugin will
+// keep running, which shows the cancel button at the bottom of the screen.
+figma.closePlugin();
